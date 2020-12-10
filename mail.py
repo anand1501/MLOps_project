cd8b8052dda787d57e3315e26d873acc37423856
anand1501 <60420105+anand1501@users.noreply.github.com>
2020-12-10 10:59:50 +0530

    final commit

--- mail.py
+++ mail.py
@@ -5,6 +5,6 @@ s.login("anandkr.1501@gmail.com", "3179724774937007")
     # message
 message1 = "accuracy is less than 90%.Try again"
     # sending the mail 
-s.sendmail("anand31798@gmail.com", "1706298@kiit.ac.in", message1)
+s.sendmail("anandkr.1501@gmail.com", "1706298@kiit.ac.in", message1)
     # terminating the session 
 s.quit()

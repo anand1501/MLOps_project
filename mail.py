b8e4dd86856ee66305730a49e18741bcc4d7d76e
anand1501 <60420105+anand1501@users.noreply.github.com>
2020-12-10 11:23:01 +0530

    final commit

--- mail.py
+++ mail.py
@@ -1,10 +1,10 @@
 import smtplib
 s = smtplib.SMTP('smtp.gmail.com', 587)
 s.starttls()
-s.login("anandkr.1501@gmail.com", "3179724774937007")
+s.login("anand31798@gmail.com", "3179724774937007")
     # message
 message1 = "accuracy is less than 90%.Try again"
     # sending the mail 
-s.sendmail("anandkr.1501@gmail.com", "1706298@kiit.ac.in", message1)
+s.sendmail("anand31798@gmail.com", "1706298@kiit.ac.in", message1)
     # terminating the session 
 s.quit()
